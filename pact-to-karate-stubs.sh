ab56ba656188bdb036ba0563ab7b2038a37de2be
David Mitchell <davmitchell@deloitte.com.au>
2019-08-12 13:13:31 +1000

    First cut of pact-to-karate-stubs.sh

---
+++ pact-to-karate-stubs.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+INPUT=$(echo $(cat))
+
+CONSUMER=$(echo $INPUT |
+    jq '.consumer.name')
+
+PROVIDER=$(echo $INPUT |
+    jq '.provider.name')
+
+NUM_INTERACTIONS=$(echo $INPUT |
+    jq '.interactions | length')
+
+echo "Feature: Provider $PROVIDER responding to consumer $CONSUMER"
+echo ""
+echo "Background:"
+echo "  * configure cors = true"
+echo ""
+for INTERACTION in $(seq $NUM_INTERACTIONS)
+do
+    #echo $INTERACTION
+    I="$(($INTERACTION - 1))"
+    DESCRIPTION=$(echo $INPUT | 
+        jq --raw-output --argjson i "$I" '.interactions[$i].description') 
+
+    URL=$(echo $INPUT |
+        jq --raw-output --argjson i "$I" '.interactions[$i].request.path') 
+
+    METHOD=$(echo $INPUT |
+        jq --raw-output --argjson i "$I" '.interactions[$i].request.method | ascii_downcase') 
+
+    REQUEST_BODY=$(echo $INPUT |
+        jq -c --raw-output --argjson i "$I" '.interactions[$i].request.body')
+
+    STATUS=$(echo $INPUT |
+        jq --raw-output --argjson i "$I" '.interactions[$i].response.status')
+
+    RESPONSE_BODY=$(echo $INPUT |
+        jq -c --raw-output --argjson i "$I" '.interactions[$i].response.body') 
+
+    if [ "$REQUEST_BODY" != 'null' ]; then
+      echo "Scenario: pathMatches('${URL}') && methodIs('${METHOD}') && request(${REQUEST_BODY})"
+    fi
+    if [ "$RESPONSE_BODY" != 'null' ]; then echo "    * def response = $RESPONSE_BODY"; fi
+    if [ "$STATUS" != 'null' ]; then echo "    * def responseStatus = $STATUS"; fi
+    echo ""
+done
+
+echo "Scenario:"
+echo "    def responseStatus = 404"
